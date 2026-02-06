5f580898e0dcf6c6609008b4186ce78b64e578b4
Cyphire2025 <cyphire1@gmail.com>
2026-02-06 20:26:57 +0530

    chore: setup for coolify deployment

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:20-alpine
+
+WORKDIR /app/medusa
+
+COPY package.json .
+COPY yarn.lock .
+
+RUN yarn install
+
+COPY . .
+
+RUN yarn build
+
+EXPOSE 9000
+
+CMD ["yarn", "start"]
---
+++ medusa-config.js
@@ -0,0 +1,21 @@
+const { loadEnv, defineConfig } = require('@medusajs/framework/utils');
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd());
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
+    http: {
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+    workerMode: process.env.MEDUSA_WORKER_MODE || "shared",
+  },
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+  }
+});
